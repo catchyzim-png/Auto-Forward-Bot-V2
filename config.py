46bcd9434d8dc0e40254e63c436d01d314013f01
catchyzim-png <catchyzim@gmail.com>
2026-06-03 11:58:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
-from os import environ 
+from os import environ
 
 class Config:
-    API_ID = environ.get("API_ID", "577678")
+    # Service credentials fetched from environment variables
+    API_ID = int(environ.get("API_ID", "577678"))
     API_HASH = environ.get("API_HASH", "d2c6e01uuiuiouioiuiou0fc6d7a1be")
     BOT_TOKEN = environ.get("BOT_TOKEN", "70955...") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
+    
+    # FIX: Checks both "DATABASE_URI" and "DATABASE" so it adapts to your Hugging Face Secret naming
+    DATABASE_URI = environ.get("DATABASE_URI", environ.get("DATABASE", ""))
     DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6964148334').split()]
+    
+    # Owner ID setup
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6463734458').split()]
 
 class temp(object): 
     lock = {}
@@ -15,4 +20,3 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-    
